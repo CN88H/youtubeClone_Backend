ab223f9b555383617de02704e66068abbc8a7c13
CN88H <cheenou88@gmail.com>
2022-08-22 21:39:51 -0500

    Created Local Settings and attempting to connect to database.

---
+++ backend/drf_jwt_backend/local_setting.py
@@ -0,0 +1,11 @@
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'youtube_clone_database',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'Damnbaka.88'
+    }
+}
--- backend/drf_jwt_backend/settings.py
+++ backend/drf_jwt_backend/settings.py
@@ -124,6 +124,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',)
 }
 
+
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=10),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=20),
